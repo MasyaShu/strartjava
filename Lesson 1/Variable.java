f22f54889afe6c55a4ae6c61809582832eac0938
mShu <masyashu@gmail.com>
2020-03-02 14:58:03 +0500

    перенос в папку

---
+++ Lesson 1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {	
+    public static void main(String[] args) {
+    	byte numCores = 7;
+    	short numMonitor = 2;
+    	int numMemoryMb = 16384;
+    	long numMemoryKb = 16777216;
+    	float monitorDiagonalInch = 23.8f;
+    	double monitorDiagonalCm = 60.452d;
+    	char proc = 'A';
+    	boolean isGoodComputer = true;
+
+        System.out.println("Количество ядер: " + numCores);
+        System.out.println("Количество мониторов: " + numMonitor);
+        System.out.println("Количество оперативной памяти Mb: " + numMemoryMb);
+        System.out.println("Количество оперативной памяти Kb: " + numMemoryKb);
+        System.out.println("Диагональ монитора дюйм: " + monitorDiagonalInch);
+        System.out.println("Диагональ монитора см: " + monitorDiagonalCm);
+        System.out.println("Первая буква производителя процессора: " + proc);
+        System.out.println("Это хороший компьютер? : " + isGoodComputer);
+    }
+}
