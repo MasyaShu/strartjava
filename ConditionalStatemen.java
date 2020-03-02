d5e49c6563ddf6673e931d932a3cc0bd3a850ec4
mShu <masyashu@gmail.com>
2020-03-02 14:43:10 +0500

    Второе ДЗ, попытка 2

--- ConditionalStatemen.java
+++ ConditionalStatemen.java
@@ -1,31 +1,36 @@
 public class ConditionalStatemen {
 	public static void main(String[] args) {
 		int age = 10;
-		boolean isMale = true;
-		float height = 1.79f;
-		char firstLetterName = 'V';
-
+		
 		if(age > 20) {
 			System.out.println("Возврат больше 20");
 		}
+
+		boolean isMale = true;
 
 		if(isMale) {
 			System.out.println("Это мужчина");
 		}
 
+		if(!isMale) {
+			System.out.println("Это женщина");
+		}
+
+		float height = 1.78f;
+
 		if(height < 1.80) {
 			System.out.println("Ваш рост меньше 1.80");
-		}
-		else {
+		} else {
 			System.out.println("Ваш рост больше 1.80");
 		}
+
+		char firstLetterName = 'V';
 		
 		if(firstLetterName == 'M') {
 			System.out.println("Первая буква M");
 		} else if(firstLetterName == 'I') {
 			System.out.println("Первая буква I");
-		}
-		else {
+		} else {
 			System.out.println("Первая буква не M и не I");
 		}
 
