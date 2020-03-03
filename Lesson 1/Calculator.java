58bb511a017a6e3f5587a48e6cf84644c0023673
mShu <masyashu@gmail.com>
2020-03-03 12:54:52 +0500

    Домашнее задание 1 лекция (попытка 2)

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
 		} else if(expression == '-') {
 			result = a - b;
 		} else if(expression == '/') {
-			if(b!=0){
+			if(b != 0){
 				result = a / b;
 			}
 		} else if(expression == '*') {
@@ -19,10 +19,9 @@ public class Calculator {
 			result = a % b;
 		} else if(expression == '^') {
 			result = a;
-			for(int i = 2; i <= b; i++)
-				{
-					result = result * a;
-				}
+			for(int i = 2; i <= b; i++) {
+				result *= a;
+			}
 		}
 		System.out.println(result);
 	}
