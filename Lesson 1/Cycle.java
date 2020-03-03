ed2d49bf4a781c438efad3b08e61a689eb9f3ac4
mShu <masyashu@gmail.com>
2020-03-03 08:42:58 +0500

    DZ Cycle 2

--- Lesson 1/Cycle.java
+++ Lesson 1/Cycle.java
@@ -1,22 +1,23 @@
 public class Cycle {
 	public static void main(String[] args) {
-		for(int i=0; i <= 20; i++) {
+		for(int i = 0; i <= 20; i++) {
 			System.out.println(i);
 		}
 
 		int j = 6;
 		while(j >= -6) {
 			System.out.println(j);
-			j = j - 2;
+			j -= 2;
 		}
 
 		int value = 10;
+		int sum = 0;
 		do {
 			if(value % 2 > 0) {
-				System.out.println(value);
+				sum += value;
 			}
 			value = value + 1;
-		}
-		while(value <= 20);
+		} while(value <= 20);
+		System.out.println(sum);
 	}
 }
