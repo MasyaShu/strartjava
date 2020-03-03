41df9c87eb43343be880b6fe06a314f754f260d8
mShu <masyashu@gmail.com>
2020-03-03 16:23:52 +0500

    Домашнее задание 1 лекция (попытка 3)

--- Lesson 1/MyFirstGame.java
+++ Lesson 1/MyFirstGame.java
@@ -1,20 +1,19 @@
 public class MyFirstGame {
 	public static void main(String[] args) {
 		int input = 1;
-		int secret = 19;
-		boolean gameOver = false;
+		int secretNum = 19;
 		
-		while(!gameOver) {
-			if(input > secret) {
-			System.out.println("Введенное вами число больше того, что загадал компьютер");
-			input -= 1;
-			} else if(input < secret) {
-			System.out.println("Введенное вами число меньше того, что загадал компьютер");
-			input += 1;
+		while(true) {
+			if(input > secretNum) {
+				System.out.println("Введенное вами число больше того, что загадал компьютер");
+				input--;
+			} else if(input < secretNum) {
+				System.out.println("Введенное вами число меньше того, что загадал компьютер");
+				input++;
 			} else {
-			System.out.println("Вы угадали!");
-			gameOver = true;
+				break;
 			}
 		}
+		System.out.println("Вы угадали!");
 	}
 }
