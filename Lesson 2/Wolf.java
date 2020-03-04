02b792b5fd89f7f8b2b49b2d9d9f3d12c63ee473
mShu <masyashu@gmail.com>
2020-03-04 14:33:39 +0500

    Домашнее задание 2 лекция, пункт 3

---
+++ Lesson 2/Wolf.java
@@ -0,0 +1,28 @@
+public class Wolf {
+
+	boolean gender; //true M, false F
+	String nickname;
+	float weight;
+	int age;
+	String color;
+
+	void go() {
+		System.out.println("Идем");
+	}
+
+	void sit() {
+		System.out.println("Сели");
+	}
+
+	void run() {
+		System.out.println("Бежим");
+	}
+
+	void howl() {
+		System.out.println("Воем");
+	}
+
+	void hunt() {
+		System.out.println("Охотимся");
+	}
+}
---
+++ Lesson 2/WolfTest.java
@@ -0,0 +1,26 @@
+public class WolfTest {
+
+	public static void main(String[] args) {
+		
+		Wolf wolf = new Wolf();
+		wolf.gender = false;
+		wolf.nickname = "Barbos";
+		wolf.weight = 13.4f;
+		wolf.age = 4;
+		wolf.color = "Черный";
+
+		if(wolf.gender)
+			System.out.println("Пол: male");
+		else
+			System.out.println("Пол: female");
+		System.out.println("Кличка: " + wolf.nickname);
+		System.out.println("Вес: " + wolf.weight);
+		System.out.println("Возвраст: " + wolf.age);
+		System.out.println("Окрас: " + wolf.color);
+		wolf.go();
+		wolf.sit();
+		wolf.run();
+		wolf.howl();
+		wolf.hunt();
+	}
+}
