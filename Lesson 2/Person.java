2bd9ef8669057f8f0d4d34147b446f628e64fbca
mShu <masyashu@gmail.com>
2020-03-04 14:09:39 +0500

    Домашнее задание 2 лекция, пункт 2

---
+++ Lesson 2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+	
+	boolean sex = true; //true M, false W
+	String mame = "Ivan";
+	float height = 1.83f;
+	float weight = 88.3f
+	int age = 20;
+
+	void go() {
+		System.out.println("Идем");
+	}
+
+	void sit() {
+		System.out.println("Сели");
+	}
+
+	void run() {
+		System.out.println("Бежим");
+	}
+
+	void talk() {
+		System.out.println("Говорим");
+	}
+
+	void learnJava() {
+		System.out.println("Учим Java");
+	}
+}
