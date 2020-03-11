3ec7ad978d5a606decc92db46a464cc28047e2c0
mShu <masyashu@gmail.com>
2020-03-11 22:19:46 +0500

    Домашнее задание 2 лекция, пункт 6 (попытка 4)

--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -5,6 +5,7 @@ public class CalculatorTest {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String reply = "Y";
+		
 		while(reply.equalsIgnoreCase("Y")) {
 			Calculator calculator = new Calculator();
 			System.out.print("Введите первое число: ");
