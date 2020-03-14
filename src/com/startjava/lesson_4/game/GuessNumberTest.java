d2a3635d8ff2859835d6eb74f37c2ac90b8986f6
mShu <masyashu@gmail.com>
2020-03-14 10:06:38 +0500

    lesson 4

--- src/com/startjava/lesson_4/game/GuessNumberTest.java
+++ src/com/startjava/lesson_4/game/GuessNumberTest.java
@@ -1,4 +1,8 @@
 package com.startjava.lesson_4.game;
 
 public class GuessNumberTest {
+
+    public static void main(String[] args) {
+        System.out.print("Хотите продолжить? [Y/N]: ");
+    }
 }
