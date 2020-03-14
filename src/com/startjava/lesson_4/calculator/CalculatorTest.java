8bbb7c043b27c7c8262ab43878d779b591d6cd06
mShu <masyashu@gmail.com>
2020-03-14 17:20:31 +0500

    lesson 4

--- src/com/startjava/lesson_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_4/calculator/CalculatorTest.java
@@ -1,4 +1,27 @@
 package com.startjava.lesson_4.calculator;
 
+import java.util.Scanner;
+
 public class CalculatorTest {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String reply = "Y";
+
+        while(reply.equalsIgnoreCase("Y")) {
+            Calculator calculator = new Calculator();
+            System.out.print("Введите первое число: ");
+            calculator.setA(scan.nextInt());
+            System.out.print("Введите знак математической операции: ");
+            calculator.setSing(scan.next().charAt(0));
+            System.out.print("Введите второе число: ");
+            calculator.setB(scan.nextInt());
+            System.out.println(calculator.calculate());
+
+            do {
+                System.out.print("Хотите продолжить? [Y/N]: ");
+                reply = scan.next();
+            } while(!(reply.equalsIgnoreCase("N") || reply.equalsIgnoreCase("Y")));
+        }
+    }
 }
