05f04a6a341f7c65ec7748f18fab3efd3d072e19
mShu <masyashu@gmail.com>
2020-03-15 15:46:58 +0500

    lesson 4

--- src/com/startjava/lesson_4/game/GuessNumber.java
+++ src/com/startjava/lesson_4/game/GuessNumber.java
@@ -1,6 +1,5 @@
 package com.startjava.lesson_4.game;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class GuessNumber {
@@ -9,7 +8,7 @@ public class GuessNumber {
     private Player player1;
     private Player player2;
 
-    GuessNumber (Player player1, Player player2) {
+    GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
     }
